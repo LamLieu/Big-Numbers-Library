5b480fb87daed7d2d891c52e189c2ec80ad1780d
LamLieu <LamLieu@users.noreply.github.com>
2018-05-31 18:43:55 -0700

    Adding dynamic library code and dll file

---
+++ BigNumbersLibrary_dynamic.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifdef BIGNUMBERSLIBRARY_EXPORTS  
+#define BIGNUMBERSLIBRARY_API __declspec(dllexport)   
+#else  
+#define BIGNUMBERSLIBRARY_API __declspec(dllimport)   
+#endif  
+
+#pragma once
+#include "stdafx.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class BigNumber {
+private:
+	int digits;
+	bool positive = true;
+	vector<int> numberVector;
+
+public:
+	// Constructors
+	BigNumber();
+	BigNumber(int);
+	BigNumber(string);
+	BigNumber(vector<int>);
+
+	// Addition
+	BigNumber operator+(BigNumber const &);
+
+	// Subtraction
+	BigNumber operator-(BigNumber const &);
+
+	// Multiplication
+	BigNumber operator*(BigNumber const &);
+
+	// Division
+	BigNumber operator/(BigNumber const &);
+
+	// Mod
+	BigNumber operator%(BigNumber const &);
+
+	BigNumber operator=(BigNumber const &);
+
+	void print();
+};
